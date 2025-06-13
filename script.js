61998e7ab2ca41602d82b65876a4c384b2ce41e2
PurviMurkute <purvimurkute51@gmail.com>
2025-06-13 23:52:57 +0530

    Logic implemented

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const hmoveContainer = document.querySelector('#human-move')
-const cmoveContainer = document.querySelector('#computer-move')
+const hmoveContainer = document.querySelector("#human-move");
+const cmoveContainer = document.querySelector("#computer-move");
+const winningMsg = document.querySelector("#winning-msg");
+const hScoreContainer = document.querySelector("#h-Score")
+const cScoreContainer = document.querySelector("#c-Score")
 
-function cumputerMove(){
-    const moves = ["rock", "paper", "scissor"];
-    const randomIndex = Math.floor(Math.random() * moves.length) + 1;
-    console.log(randomIndex);
-    cmoveContainer.innerText = moves[randomIndex];
+function computerMove() {
+  const moves = ["rock", "paper", "scissor"];
+  const randomIndex = Math.floor(Math.random() * moves.length);
+  return moves[randomIndex];
 }
 
-function playMove(move){
-    hmoveContainer.innerText = move;
-    cumputerMove()
+function playMove(humanMove) {
+  const computerTurn = computerMove();
+  hmoveContainer.innerText = humanMove;
+  cmoveContainer.innerText = computerTurn;
+  console.log(computerTurn);
+
+  if (humanMove === computerTurn) {
+    winningMsg.innerText = "Draw";
+  } else if (
+    (humanMove === "rock" && computerTurn === "scissor") ||
+    (humanMove === "scissor" && computerTurn === "paper") ||
+    (humanMove === "paper" && computerTurn === "rock")
+  ) {
+    winningMsg.innerText = "You Win!";
+    hScoreContainer.innerText = parseInt(hScoreContainer.innerText) + 1; 
+  } else {
+    winningMsg.innerText = "You Lose!";
+    cScoreContainer.innerText = parseInt(cScoreContainer.innerText) + 1;
+  }
 }
